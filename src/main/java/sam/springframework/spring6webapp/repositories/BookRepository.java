6d75d4afe177cde9cc4615eca5ef3293d24787dd
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-11 18:13:32 -0300

    spring data repository

---
+++ src/main/java/sam/springframework/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package sam.springframework.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sam.springframework.spring6webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
