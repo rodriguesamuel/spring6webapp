a94a0d6a1206d651d7f57dd7b93d27cdbce7ef4b
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-11 18:35:58 -0300

    initializing data with spring

---
+++ src/main/java/sam/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,56 @@
+package sam.springframework.spring6webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import sam.springframework.spring6webapp.domain.Author;
+import sam.springframework.spring6webapp.domain.Book;
+import sam.springframework.spring6webapp.repositories.AuthorRepository;
+import sam.springframework.spring6webapp.repositories.BookRepository;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author();
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
+
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("123456");
+
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
+
+        Author rod = new Author();
+        rod.setFirstName("Rod");
+        rod.setLastName("Jhonson");
+
+        Book noEJB = new Book();
+        noEJB.setTitle("J2EE development without EJB");
+        noEJB.setIsbn("789456");
+
+        Author rodSaved = authorRepository.save(rod);
+        Book noEJBSaved = bookRepository.save(noEJB);
+
+        ericSaved.getBooks().add(dddSaved);
+        rodSaved.getBooks().add(noEJBSaved);
+
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
+
+        System.out.println("In bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+
+    }
+}
